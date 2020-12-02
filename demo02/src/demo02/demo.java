71de7b6292e0b47fbe3abecce29c7eb9f4be4a52
sunjinbiao <254272669@qq.com>
2020-12-02 17:21:53 +0800

    123

--- demo02/src/demo02/demo.java
+++ demo02/src/demo02/demo.java
@@ -6,23 +6,23 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class demo {
-	//static WebDriver driver;
+	static WebDriver driver;
 	@Test
 	
 	public  void test()throws InterruptedException {
-	System.out.println("hello");
-		//System.setProperty("webdriver.gecko.driver", "D:\\Demo\\geckodriver.exe");
-		// driver =new FirefoxDriver();
-	//    driver.get("http://localhost/index.php/admin");
+	
+		System.setProperty("webdriver.gecko.driver", "D:\\Demo\\geckodriver.exe");
+		 driver =new FirefoxDriver();
+	    driver.get("http://localhost/index.php/admin");
 		
-		//Thread.sleep(3000);
-		//driver.findElement(By.name("user")).sendKeys("admin");
-		//driver.findElement(By.name("password")).sendKeys("admin");
-		//Thread.sleep(3000);
-		//driver.findElement(By.name("login_submit")).click();
-		//Thread.sleep(2000);
-		//driver.findElement(By.linkText("配置")).click();
-		//Thread.sleep(1000);
+		Thread.sleep(3000);
+		driver.findElement(By.name("user")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("admin");
+		Thread.sleep(3000);
+		driver.findElement(By.name("login_submit")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("配置")).click();
+		Thread.sleep(1000);
 	}
 
 
