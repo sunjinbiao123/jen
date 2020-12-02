0fe13618c3fb60b74b25bad6a299f75015965043
sunjinbiao <254272669@qq.com>
2020-12-02 14:09:32 +0800

    6

--- demo02/src/demo02/demo.java
+++ demo02/src/demo02/demo.java
@@ -1,13 +1,29 @@
 package demo02;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class demo {
+	static WebDriver driver;
 	@Test
-	public void test() {
+	
+	public  void test()throws InterruptedException {
+		System.setProperty("webdriver.gecko.driver", "D:\\Demo\\geckodriver.exe");
+		 driver =new FirefoxDriver();
+	    driver.get("http://localhost/index.php/admin");
 		
-				System.out.println("hello,world");
+		Thread.sleep(3000);
+		driver.findElement(By.name("user")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("sjb123456");
+		Thread.sleep(3000);
+		driver.findElement(By.name("login_submit")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("配置")).click();
+		Thread.sleep(1000);
 	}
+
 
 		
 
