0f95fe307159fc53e304957eab666029c1afaa4e
sunjinbiao <254272669@qq.com>
2020-12-03 09:58:52 +0800

    2

--- demo04/src/demo04/Demo.java
+++ demo04/src/demo04/Demo.java
@@ -1,10 +1,22 @@
 package demo04;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Demo {
+	WebDriver driver;
 	@Test
 	public void test() {
-		System.out.print("helloworld");
+		System.setProperty("webdriver.gecko.driver", "D:\\Demo\\geckodriver.exe");
+		 driver =new FirefoxDriver();
+		 driver.get("http://localhost/index.php/admin");
+		
+		Thread.sleep(3000);
+		driver.findElement(By.name("user")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("admin");
+		Thread.sleep(3000);
+		driver.findElement(By.name("login_submit")).click();
+		driver.quit();
 	}
 }
