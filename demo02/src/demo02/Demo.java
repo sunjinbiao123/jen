3e9129718e55f60a019f3527a9c8de577f37b61e
sunjinbiao <254272669@qq.com>
2020-12-03 08:53:04 +0800

    3

--- demo02/src/demo02/Demo.java
+++ demo02/src/demo02/Demo.java
@@ -9,8 +9,8 @@ public class Demo {
 	WebDriver driver;
 	@Test
 	public  void test()throws InterruptedException {
-		System.out.println("hello");
-		/*System.setProperty("webdriver.gecko.driver", "C:\\Demo\\geckodriver.exe");
+		//System.out.println("hello");
+		System.setProperty("webdriver.gecko.driver", "C:\\Demo\\geckodriver.exe");
 		 driver =new FirefoxDriver();
 		 driver.get("http://localhost/index.php/admin");
 		
@@ -22,6 +22,6 @@ public class Demo {
 		Thread.sleep(2000);
 		driver.findElement(By.linkText("配置")).click();
 		Thread.sleep(1000);
-		driver.quit();*/
+		driver.quit();
 	}
 }
