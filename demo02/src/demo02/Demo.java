c414967f5d596b56ba5469c7a1cc4a75365a4885
sunjinbiao <254272669@qq.com>
2020-12-03 09:03:15 +0800

    1

--- demo02/src/demo02/Demo.java
+++ demo02/src/demo02/Demo.java
@@ -1,4 +1,4 @@
-﻿package demo02;
+﻿
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,7 +8,7 @@ public class Demo {
 	@Test
 	public  void test()throws InterruptedException {
 		System.out.println("HEllo");	
-		*/System.setProperty("webdriver.gecko.driver", "C:\\Demo\\geckodriver.exe");
+		/*System.setProperty("webdriver.gecko.driver", "C:\\Demo\\geckodriver.exe");
 		 driver =new FirefoxDriver();
 		 driver.get("http://localhost/index.php/admin");
 		Thread.sleep(3000);
